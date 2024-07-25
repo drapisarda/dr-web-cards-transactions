905ef8179ac250efafd81c2f5508844626169dff
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-25 18:59:56 +0200

    transaction component

---
+++ src/components/Transaction.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps({
+  description: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div class="transaction px-7 py-9 bg-gray-300 flex rounded-md">
+    <div class="transaction__description basis-4/5">
+      {{ description }}
+    </div>
+    <div class="transaction__amount">
+      {{ amount.toString().replace('.', ',') }}€
+    </div>
+  </div>
+
+</template>
+
+<style lang="scss"></style>
