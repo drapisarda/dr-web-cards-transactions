65355e66dbb2ee31fdf2b0b914414fddd7bec9db
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-25 19:12:10 +0200

    transaction list component

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import transactions from '../testdata/transactions.json'
+import Transaction from './Transaction.vue';
+
+const transactionList = transactions["lkmfkl-mlfkm-dlkfm"]
+
+</script>
+
+<template>
+  <div class="translation-list">
+    <div class="container">
+      <div class="translaction-list__item mb-5" v-for="(item) in transactionList" :key="item.id">
+        <Transaction :description="item.description" :amount="item.amount" />
+      </div>
+    </div>
+  </div>
+</template>
