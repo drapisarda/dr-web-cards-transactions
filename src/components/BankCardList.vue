65c781bacad4d0db2f8bb57e4cda16974c10bae9
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-25 21:11:58 +0200

    BanKCardList component

---
+++ src/components/BankCardList.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { Card } from '@/types/types';
+import type { PropType } from 'vue';
+import BankCard from './BankCard.vue';
+
+defineProps({
+  cards: {
+    type: Object as PropType<Card[]>,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <div class="bank-card-list mb-7">
+    <div class="container flex justify-between">
+      <div class="bank-card-list__item mb-2 basis-2/5" v-for="(item, index) in cards" :key="index">
+        <BankCard :description="item.description" :id="item.id" />
+      </div>
+    </div>
+  </div>
+</template>
