fd179539d05e23bc4b512f25f7dea3f3c7c389c7
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-25 18:39:46 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.vue'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
-
