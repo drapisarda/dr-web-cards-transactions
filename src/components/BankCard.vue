f14d80aad07e6969ac647672bf154c637f8ae797
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-25 21:11:36 +0200

    BankCard component

---
+++ src/components/BankCard.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { type CardType } from '../types/types'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+})
+
+// TODO there would be nice a proper field
+const cardType = props.description.replace(' Card', '').toLowerCase()
+</script>
+
+<template>
+  <div
+    :class="[`bank-card bank-card--${cardType} rounded-xl bg-gray-300 hover:bg-blue-200 px-5 py-2 pb-20 cursor-pointer`]">
+    <div class="bank-card__id font-semibold">{{ id }}</div>
+    <div class="bank-card__description">{{ description }}</div>
+  </div>
+</template>
