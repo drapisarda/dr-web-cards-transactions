3b44185cb923721e6dc9cd1ad7729a855a7dbf87
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-26 12:40:37 +0200

    test include setting

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,8 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
-        provider: 'v8'
+        provider: 'v8',
+        include: ['src']
       },
     },
   })
