c3f4ff2730a3056823c4a4d04917d78f94835ae4
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-26 14:00:24 +0200

    removed unused styles from App

--- src/App.vue
+++ src/App.vue
@@ -57,32 +57,3 @@ const filterBehaviour = async ({ target }: { target: HTMLInputElement }) => {
     <TransactionList :transactions="transactions" />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
