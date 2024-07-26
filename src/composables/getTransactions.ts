a75989dc0005830364acbf00ab109d3e7ac1709c
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-26 18:44:02 +0200

    extended transactions

--- src/composables/getTransactions.ts
+++ src/composables/getTransactions.ts
@@ -6,7 +6,7 @@ export default async function getTransactions(cardId: string, amountFilterValue:
     throw new Error("Invalid card selected")
   }
   
-  const transactionsModule = await import('../data/transactions.json' as any)
+  const transactionsModule = await import('../data/transactions_extended.json' as any)
   const transactionsContainer : TransactionsContainer = transactionsModule.default
 
   if (cardId in transactionsContainer) {
