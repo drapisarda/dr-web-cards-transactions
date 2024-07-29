d84581109f3f1c38208dd6d44deaaddcf65b197c
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-29 11:42:52 +0200

    tests for placeholder in TransactionContent

--- src/__tests__/TransactionContent.spec.ts
+++ src/__tests__/TransactionContent.spec.ts
@@ -8,7 +8,15 @@ describe('BankCard', () => {
     const description = 'This is a transaction'
     const amount = 99.99
     const wrapper = mount(TransactionContent, { props: { description, amount } })
+    expect(wrapper.classes()).not.toContain('shiner')
     expect(wrapper.text()).toContain(description)
     expect(wrapper.text()).toContain(`${amount.toString().replace('.', ',')}€`)
+  })
+
+  it('renders as placeholder', () => {
+    const description = '#'
+    const amount = 0
+    const wrapper = mount(TransactionContent, { props: { description, amount } })
+    expect(wrapper.classes()).toContain('shiner')
   })
 })
