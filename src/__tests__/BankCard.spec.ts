27fb1b955b36b94606f128434a7104dfccc8b3e8
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-29 11:44:16 +0200

    Test BankCard placeholder UI

--- src/__tests__/BankCard.spec.ts
+++ src/__tests__/BankCard.spec.ts
@@ -8,7 +8,15 @@ describe('BankCard', () => {
     const cardId = '99'
     const cardDescription = 'Lorem ipsum dolor sit amet'
     const wrapper = mount(BankCard, { props: { id: cardId, description: cardDescription } })
+    expect(wrapper.classes()).not.toContain('shiner')
     expect(wrapper.text()).toContain(cardId)
     expect(wrapper.text()).toContain(cardDescription)
+  })
+
+  it('renders properly as placeholder', () => {
+    const cardId = '#'
+    const cardDescription = '#'
+    const wrapper = mount(BankCard, { props: { id: cardId, description: cardDescription } })
+    expect(wrapper.classes()).toContain('shiner')
   })
 })
